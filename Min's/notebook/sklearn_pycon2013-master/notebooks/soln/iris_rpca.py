8d8a589ced05afd385b6a99264b8a90620ca50b4
blueoxss <jeongmingi@jeongmingiui-MacBook-Air.local>
2013-08-30 04:14:36 +0900

    MIN'S
    MIN'S PROJECT~

--- Min's/notebook/sklearn_pycon2013-master/notebooks/soln/iris_rpca.py
+++
@@ -1,9 +0,0 @@
-from sklearn.decomposition import RandomizedPCA
-
-X_rpca = RandomizedPCA(n_components=2).fit_transform(X)
-
-plot_PCA_2D(X_rpca, iris.target, iris.target_names)
-plt.title('Randomized PCA')
-
-plot_PCA_2D(X_pca, iris.target, iris.target_names)
-plt.title('PCA')
