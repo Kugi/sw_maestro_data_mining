f50c15390f214f04dd2ee5571deb834f498de28f
jeongmingi <blueoxss13@gmail.com>
2013-10-22 03:33:57 +0900

    frontend_update fronted0.1v…

---
+++ frontend_update/app/js/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/* App Module */
+
+angular.module('appcat', [
+        'appcatFilters',
+        'appcatServices',
+        'graph.directives',
+        'dangle',
+        'appcat.controllers']).
+  config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+      when('/apps', {templateUrl: 'app/partials/app-list.html',   controller: AppListCtrl}).
+      when('/apps/:appId', {templateUrl: 'app/partials/app-detail.html', controller: AppDetailCtrl}).
+      when('/apps/:appId/detail', {templateUrl: 'app/partials/detail.html', controller: DetailCtrl}).
+      otherwise({redirectTo: '/apps'});
+}]).config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common["X-Requested-With"];
+        $httpProvider.defaults.headers.common["Accept"] = "application/json, text/plain, * / *";
+        $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+    }]);
+
+angular.module('d3', []);
+angular.module('graph.directives', ['d3']); //module direcitves의 graph.~~모듈에 d3가 종속되있다.
+//angular.module('myApp.directives'. ['d3']);
+angular.module('appcat.controllers', [])
+    .controller('DemoCtrl', function($scope) {
+        'use strict';
+    });
